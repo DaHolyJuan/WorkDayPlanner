6322230f62b95bde5aebce599088a0b9315221a3
DaHolyJuan <juan.3scalant3@gmail.com>
2020-10-01 21:41:16 -0500

    added an hourupdater function

--- calendar.js
+++ calendar.js
@@ -1,3 +1,25 @@
 $(document).ready(function (){
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 })
+
+function hourUpdater() {
+ var currentHour = moment().hours();
+    console.log(currentHour);
+    $(".time-block").each(function() {
+        var rowHour = parseInt($(this).attr("id"));
+        console.log(rowHour);
+
+        if (rowHour < currentHour){
+            $(this).addClass("past");
+        } else if (rowHour === currentHour) {
+            $(this).removeClass("past")
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("past");
+        }
+    })
+}
+hourUpdater();
+
